3553acc4a2aaa3c179ab68e6ae8e805117a3f040
Equinox-13 <quaid.johar33@gmail.com>
2020-05-27 22:09:16 +0530

    Used relativedelta to fetch all time parameters

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from dateutil.relativedelta import relativedelta
 from datetime import datetime
 
 
@@ -9,16 +10,23 @@ app = Flask(__name__)
 
 @app.route('/',)
 def calculate_duration():
-    while True:
-        day = str((datetime.now() - datetime(2020,3,19)).days)
-        sec = str((datetime.now() - datetime(2020,3,19)).seconds)
-        micro = str((datetime.now() - datetime(2020,3,19)).microseconds)
-        ans = {
-            "days":day,
-            "seconds":sec,
-            "microseconds":micro,
-        }
-        return render_template('home.html', data=ans)
+    year = str(relativedelta(datetime.now(),datetime(2020,3,19)).years)
+    mon = str(relativedelta(datetime.now(),datetime(2020,3,19)).months)
+    day = str(relativedelta(datetime.now(),datetime(2020,3,19)).days)
+    hour = str(relativedelta(datetime.now(),datetime(2020,3,19)).hours)
+    minute = str(relativedelta(datetime.now(),datetime(2020,3,19)).minutes)
+    sec = str(relativedelta(datetime.now(),datetime(2020,3,19)).seconds)
+    micro = str(relativedelta(datetime.now(),datetime(2020,3,19)).microseconds)
+    res = {
+        "years":year,
+        "months":mon,
+        "days":day,
+        "hours":hour,
+        "minutes":minute,
+        "seconds":sec,
+        "microseconds":micro,
+    }
+    return render_template('home.html', data=res)
 
 
 if __name__ == '__main__':
